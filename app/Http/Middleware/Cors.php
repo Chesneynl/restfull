6520f2431c642e0954f38389e743d53619b8e05c
Chesney Buitendijk <chesneybuitendijk@gmail.com>
2018-12-20 17:03:30 +0100

    cors update 2

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,6 +15,9 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)->header('Access-Control-Allow-Origin', '*')->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        return $next($request)
+        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        ->header('Access-Control-Allow-Headers', 'application/json ' );
     }
 }
